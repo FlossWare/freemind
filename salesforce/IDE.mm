17fec756dcc68230dd6195979e020a9b0b39be51
Scot Floess <sfloess@sfloess.csb>
2010-11-08 18:23:27 -0500

    More detailed information on using the Eclipse plugin with Salesforce

--- salesforce/IDE.mm
+++ salesforce/IDE.mm
@@ -1,40 +1,115 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1273593826080" ID="ID_1361209462" LINK="Salesforce.mm" MODIFIED="1288875081189" TEXT="IDE">
+<node COLOR="#000000" CREATED="1273593826080" ID="ID_1361209462" LINK="Salesforce.mm" MODIFIED="1289257330416" TEXT="IDE">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1288636589968" ID="ID_667264552" MODIFIED="1288636592583" POSITION="right" TEXT="Eclipse">
+<node COLOR="#0033ff" CREATED="1289258065108" ID="ID_1847685842" MODIFIED="1289258085754" POSITION="right" TEXT="Eclipse">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1288636594056" ID="ID_358811930" MODIFIED="1288637184523" TEXT="http://wiki.developerforce.com/index.php/Force.com_IDE_Installation_for_Eclipse_3.5">
+<node COLOR="#00b439" CREATED="1288636589968" ID="ID_667264552" MODIFIED="1289258088378" TEXT="Setup">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<icon BUILTIN="full-1"/>
-</node>
-<node COLOR="#00b439" CREATED="1288637152839" ID="ID_384593182" MODIFIED="1288637188003" TEXT="Enable Eclipse Gallileo Repository">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-<icon BUILTIN="full-2"/>
-<node COLOR="#990000" CREATED="1288637629284" ID="ID_867599775" MODIFIED="1288637667729" TEXT="Help">
+<node COLOR="#990000" CREATED="1288637152839" ID="ID_384593182" MODIFIED="1289258088378" TEXT="Enable Eclipse Gallileo Repository">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
+<node COLOR="#111111" CREATED="1288637629284" ID="ID_867599775" MODIFIED="1289258088378" TEXT="Help">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
 </node>
-<node COLOR="#990000" CREATED="1288637635269" ID="ID_1059114302" MODIFIED="1288637670585" TEXT="Install New Software">
-<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1288637635269" ID="ID_1059114302" MODIFIED="1289258088379" TEXT="Install New Software">
+<font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="full-2"/>
 </node>
-<node COLOR="#990000" CREATED="1288637652245" ID="ID_526648655" MODIFIED="1288637673145" TEXT="Work with">
+<node COLOR="#111111" CREATED="1288637652245" ID="ID_526648655" MODIFIED="1289258088379" TEXT="Available Software Sites">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node COLOR="#111111" CREATED="1288637658112" ID="ID_1880272397" MODIFIED="1289258088379" TEXT="Gallileo">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node COLOR="#111111" CREATED="1288637674534" ID="ID_1056965628" MODIFIED="1289258088379" TEXT="Enable">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1289257353646" ID="ID_1925002686" MODIFIED="1289258088381" TEXT="Add">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#111111" CREATED="1289257370365" ID="ID_1020987179" MODIFIED="1289258088381" TEXT="Name">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node COLOR="#111111" CREATED="1289257372982" ID="ID_364355524" MODIFIED="1289257378853" TEXT="Force.com IDE"/>
+</node>
+<node COLOR="#111111" CREATED="1289257380310" ID="ID_1599796164" MODIFIED="1289258088381" TEXT="Location">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#111111" CREATED="1289257409783" ID="ID_55075376" MODIFIED="1289257412518" TEXT="http://www.adnsandbox.com/tools/ide/install/"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1289257432495" ID="ID_1329409110" MODIFIED="1289258088383" TEXT="Select">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-3"/>
-<node COLOR="#111111" CREATED="1288637658112" ID="ID_1880272397" MODIFIED="1288637662131" TEXT="Gallileo"/>
+<node COLOR="#111111" CREATED="1289257439191" ID="ID_1490895051" MODIFIED="1289258088383" TEXT="Force.com IDE">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#990000" CREATED="1288637674534" ID="ID_1056965628" MODIFIED="1288637680297" TEXT="Enable">
+</node>
+<node COLOR="#990000" CREATED="1289257450464" ID="ID_1925318332" MODIFIED="1289258088383" TEXT="Next">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-4"/>
 </node>
+<node COLOR="#990000" CREATED="1289257472600" ID="ID_507925381" MODIFIED="1289258088384" TEXT="Next">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-5"/>
 </node>
+<node COLOR="#990000" CREATED="1289257480768" ID="ID_1327756710" MODIFIED="1289258088384" TEXT="I accept the terms of the license agreement">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-6"/>
+</node>
+<node COLOR="#990000" CREATED="1289257502136" ID="ID_281368925" MODIFIED="1289258088384" TEXT="Finish">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-7"/>
+</node>
+<node COLOR="#990000" CREATED="1289257581106" ID="ID_1174228047" MODIFIED="1289258088385" TEXT="Trust certificates (select all)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-8"/>
+</node>
+<node COLOR="#990000" CREATED="1289257597426" ID="ID_758088542" MODIFIED="1289258088385" TEXT="OK">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-9"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1289258276047" ID="ID_1994913329" MODIFIED="1289258278646" TEXT="Project">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1289258280055" ID="ID_673378860" MODIFIED="1289258289398" TEXT="File">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1289258290304" ID="ID_1544859152" MODIFIED="1289258291150" TEXT="New">
+<node COLOR="#111111" CREATED="1289258292056" ID="ID_993282419" MODIFIED="1289258303774" TEXT="Force.com Project"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1289258306440" ID="ID_873294966" MODIFIED="1289258317519" TEXT="Project Name">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1289258317800" ID="ID_1808902625" MODIFIED="1289258320566" TEXT="Username">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1288636594056" ID="ID_358811930" MODIFIED="1289257523365" POSITION="right" TEXT="http://wiki.developerforce.com/index.php/Force.com_IDE_Installation_for_Eclipse_3.5">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
