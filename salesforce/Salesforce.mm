b47eb2e91feb0e435951b909678a01967a77b554
Scot P. floess <sfloess@nc.rr.com>
2010-11-20 11:07:13 -0500

    Merge from the salesforce branch

--- salesforce/Salesforce.mm
+++ salesforce/Salesforce.mm
@@ -20,6 +20,14 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
+<node COLOR="#0033ff" CREATED="1289353978406" ID="ID_382941273" MODIFIED="1289353991323" POSITION="right" TEXT="Client/API">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1289353992620" ID="ID_535044213" MODIFIED="1289354010235" TEXT="[password][security token]">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
 <node COLOR="#0033ff" CREATED="1288712674705" ID="ID_239719613" LINK="UI.mm" MODIFIED="1288875011652" POSITION="left" TEXT="UI">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
