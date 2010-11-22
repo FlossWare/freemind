a6fe5f5864018be2162d1569f9c792f115fe3e93
Scot P. Floess <sfloess@redhat.com>
2010-11-22 12:46:52 -0500

    Squashed commit of the following:
    commit 60387f1cc357a32aae1ecbe59386f2cb5a37e933
    Merge: 28beba5 04fda20
    Author: Scot P. Floess <sfloess@redhat.com>
    Date:   Mon Nov 22 12:46:11 2010 -0500
    
        Merge branch 'master' into development
    
        Conflicts:
        	Reference.mm
    
    commit 28beba5e0ebae14d8eef44521ac22a4d461bc93f
    Author: Scot P. Floess <sfloess@redhat.com>
    Date:   Mon Nov 22 10:53:28 2010 -0500
    
        Added Development related maps - specifically Git.

---
+++ development/Git.mm
@@ -0,0 +1,38 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1290436605918" ID="ID_618115719" LINK="Development.mm" MODIFIED="1290436620423" TEXT="Git">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1290436628710" ID="ID_689543646" MODIFIED="1290436635860" POSITION="right" TEXT=".gitignore">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1290436638606" ID="ID_153076373" MODIFIED="1290436653173" TEXT="Files to ignore">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1290436653798" ID="ID_856144671" MODIFIED="1290436664846" TEXT="! is negation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1290436665791" ID="ID_480894859" MODIFIED="1290436684464">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      *
+    </p>
+    <p>
+      !*.c
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1290436690014" ID="ID_883877205" MODIFIED="1290436700061" TEXT="Will ignore all files but .c files"/>
+</node>
+</node>
+</node>
+</node>
+</map>
