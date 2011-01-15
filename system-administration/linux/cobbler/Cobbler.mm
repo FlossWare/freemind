8c62257d60e5dd7e4531a80653d8369179723e77
Scot P. floess <sfloess@nc.rr.com>
2011-01-15 10:18:32 -0500

    Merging in latest from development.

---
+++ system-administration/linux/cobbler/Cobbler.mm
@@ -0,0 +1,22 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1295104454850" ID="ID_1224862914" LINK="../Linux.mm" MODIFIED="1295104577174" TEXT="Cobbler">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1295104584624" ID="ID_140239077" MODIFIED="1295104598652" POSITION="right" TEXT="Unsupported distro">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1295104599536" ID="ID_643069862" MODIFIED="1295104603708" TEXT="Edit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1295104605664" ID="ID_898157990" MODIFIED="1295104630749" TEXT="/usr/lib/python[version]/site-packages/cobbler/codes.py">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1295104636944" ID="ID_1869574255" MODIFIED="1295104650429" TEXT="VALID_OS_VERSIONS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
