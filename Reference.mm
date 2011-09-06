39fc6750d416fbb48cb26f9d647f178af9fe04f0
Scot P. floess <sfloess@nc.rr.com>
2011-09-06 14:17:21 -0400

    Forgot to checkin

--- Reference.mm
+++ Reference.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1288808822794" ID="ID_1348475184" MODIFIED="1288808855846" TEXT="Reference">
+<node COLOR="#000000" CREATED="1288808822794" ID="ID_1348475184" MODIFIED="1315236227282" TEXT="Reference">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
 <node COLOR="#0033ff" CREATED="1288808857697" ID="ID_1008189407" LINK="salesforce/Salesforce.mm" MODIFIED="1288874944121" POSITION="right" TEXT="Salesforce">
