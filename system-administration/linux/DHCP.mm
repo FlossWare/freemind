62581c4a00afc24480f6ec76741bf1c0f931604a
Scot P. Floess <sfloess@redhat.com>
2012-03-10 10:54:02 -0500

    Mapping to gracefully handle "Has an A record but no DHCID, not mine."

--- system-administration/linux/DHCP.mm
+++ system-administration/linux/DHCP.mm
@@ -7,15 +7,27 @@
 <node COLOR="#0033ff" CREATED="1311180379227" ID="ID_63640885" MODIFIED="1311180387802" POSITION="right" TEXT="New/Recreated VM">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1311180371275" ID="ID_709110243" MODIFIED="1311180395317" TEXT="Has an A record but no DHCID, not mine.">
+<node COLOR="#00b439" CREATED="1331394725077" ID="ID_822204554" MODIFIED="1331394730669" TEXT="Elegant">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331394751897" ID="ID_421303454" MODIFIED="1331394753685" TEXT="/etc/dhcp/dhcpd.conf">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1331394755407" ID="ID_1868002940" MODIFIED="1331394762213" TEXT="update-conflict-detection false;"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1331394717362" ID="ID_1978599811" MODIFIED="1331394722205" TEXT="Brute force">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1311180371275" ID="ID_709110243" MODIFIED="1331394723944" TEXT="Has an A record but no DHCID, not mine.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
 </node>
-<node COLOR="#00b439" CREATED="1311180326018" ID="ID_986947038" MODIFIED="1311180397218" TEXT="rm -rf /var/lib/dhcpd">
+<node COLOR="#990000" CREATED="1311180326018" ID="ID_986947038" MODIFIED="1331394723946" TEXT="rm -rf /var/lib/dhcpd">
 <edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-2"/>
+</node>
 </node>
 </node>
 </node>
