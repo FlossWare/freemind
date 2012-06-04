ebedef23f683ed2f7e65a5993b9cbedec17a40ae
Scot P. Floess <sfloess@redhat.com>
2012-06-04 09:18:21 -0400

    Added a JBoss link

---
+++ development/JBoss.mm
@@ -0,0 +1,19 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1333546065204" ID="ID_389662430" LINK="Development.mm" MODIFIED="1333546086045" TEXT="JBoss">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1333546092010" ID="ID_1498739845" MODIFIED="1333546093453" POSITION="right" TEXT="JMX">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1333546088662" ID="ID_1759662202" MODIFIED="1333546096793" TEXT="Logging">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1333546102528" ID="ID_1730752899" MODIFIED="1333546109499" TEXT="service=Logging,type=Log4jService">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
