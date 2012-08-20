9935b185c709ab478ccb92e7996074ed7241b76f
Scot P. Floess <sfloess@redhat.com>
2012-08-20 14:59:14 -0400

    Added mapping for scrollback settings in screen

--- system-administration/linux/Screen.mm
+++ system-administration/linux/Screen.mm
@@ -20,5 +20,27 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
+<node COLOR="#0033ff" CREATED="1345485626580" ID="ID_763472562" MODIFIED="1345485631496" POSITION="right" TEXT="Scrollback">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1345485642410" ID="ID_1777039254" MODIFIED="1345485645577" TEXT="screenrc">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1345485654129" ID="ID_108576145" MODIFIED="1345485661718" TEXT="defscrollback [total lines]">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1345485646169" ID="ID_1111860860" MODIFIED="1345485662859" TEXT="defscrollback 5000">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1345485665907" ID="ID_1831879660" MODIFIED="1345485684878" TEXT="command">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1345485836232" ID="ID_752251061" MODIFIED="1345485889797" TEXT="ctrl-a: scrollback [number of lines]">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1345485849794" ID="ID_893326673" MODIFIED="1345485893867" TEXT="ctrl-a: scrollback 5000"/>
+</node>
+</node>
+</node>
 </node>
 </map>
