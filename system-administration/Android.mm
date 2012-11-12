9a956d42a2d399070c909736de4f902de4447f30
Scot P. Floess <sfloess@redhat.com>
2012-11-12 14:21:11 -0500

    Added Android maps

---
+++ system-administration/Android.mm
@@ -0,0 +1,22 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1352747931659" ID="ID_122468340" LINK="System%20Administration.mm" MODIFIED="1352747952989" TEXT="Android">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1352747955856" ID="ID_1364718079" MODIFIED="1352747962063" POSITION="right" TEXT="Automatic Software Updates">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352747963156" ID="ID_53219904" MODIFIED="1352747973159" TEXT="Playstore">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#00b439" CREATED="1352747967811" ID="ID_1921327621" MODIFIED="1352747974799" TEXT="Settings">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+</node>
+</map>
--- system-administration/System Administration.mm
+++ system-administration/System Administration.mm
@@ -3,6 +3,14 @@
 <node COLOR="#000000" CREATED="1270310396325" ID="ID_251328639" LINK="../Reference.mm" MODIFIED="1288889636984" TEXT="System Administration">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1352747948168" ID="ID_1559015291" LINK="Android.mm" MODIFIED="1352747948170" POSITION="right" TEXT="Android">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1327003827279" ID="ID_1749573106" LINK="DNS-320.mm" MODIFIED="1352747992573" POSITION="right" TEXT="DNS-320">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
 <node COLOR="#0033ff" CREATED="1270222854170" ID="ID_219128712" LINK="linux/Linux.mm" MODIFIED="1288889330896" POSITION="right" TEXT="Linux">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
@@ -11,11 +19,7 @@
 <edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1269099162834" ID="ID_48147952" LINK="windows/Windows.mm" MODIFIED="1288889554033" POSITION="right" TEXT="Windows">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-</node>
-<node COLOR="#0033ff" CREATED="1327003827279" ID="ID_1749573106" LINK="DNS-320.mm" MODIFIED="1327003827283" POSITION="right" TEXT="DNS-320">
+<node COLOR="#0033ff" CREATED="1269099162834" ID="ID_48147952" LINK="windows/Windows.mm" MODIFIED="1352747990399" POSITION="right" TEXT="Windows">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
