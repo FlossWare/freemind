f7d0a0a952f534d78fba73c6cb418b609072787a
Scot P. Floess <sfloess@redhat.com>
2012-12-07 18:28:13 -0500

    Added additional information for DVD work

--- system-administration/linux/DVD.mm
+++ system-administration/linux/DVD.mm
@@ -10,10 +10,30 @@
 <node COLOR="#00b439" CREATED="1354890550663" ID="ID_354136969" MODIFIED="1354890559732" TEXT="Multi movies/disk">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1354890561255" ID="ID_1549604656" MODIFIED="1354890590692" TEXT="mplayer dvd://[movie number] -v -dumpstream -dumpfile [name].vob">
+<node COLOR="#990000" CREATED="1354890561255" ID="ID_1549604656" MODIFIED="1354898031816" TEXT="mplayer dvd://[movie number] -nosub -v -dumpstream -dumpfile [name].vob">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1354890616335" ID="ID_1738759255" MODIFIED="1354890634461" TEXT="mplayer dvd://1 -v -dumpstream -dumpfile Movie1.vob ">
+<node COLOR="#990000" CREATED="1354890616335" ID="ID_1738759255" MODIFIED="1354898038448" TEXT="mplayer dvd://1 -nosub -v -dumpstream -dumpfile Movie1.vob ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1354896169782" ID="ID_1070052541" MODIFIED="1354896172690" POSITION="right" TEXT="mencoder">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1354896197766" ID="ID_1844470305" MODIFIED="1354896200867" TEXT="AVI">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1354896174342" ID="ID_1372418759" MODIFIED="1354898991765" TEXT="mencoder [name].vob  -nosub -o [name].avi  -of  avi -oac  copy lavc -lavcopts vcodec=mpeg4 -ovc lavc ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1354913389701" ID="ID_290695912" MODIFIED="1354913391081" TEXT="MPG">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1354896174342" ID="ID_668567183" MODIFIED="1354913428820" TEXT="mencoder [name].vob  -nosub -o [name].mpg  -of  avi -oac  copy lavc -lavcopts vcodec=mpeg4 -ovc lavc ">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
