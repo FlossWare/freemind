ee342fa580165eb0748133faad558a267bcbfda4
Scot P. Floess <sfloess@redhat.com>
2012-12-30 13:17:17 -0500

    Mapping setting SOAP header management

--- development/Java.mm
+++ development/Java.mm
@@ -4,14 +4,31 @@
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1344537792587" ID="ID_433610431" MODIFIED="1344537796608" POSITION="right" TEXT="wsimport">
+<node COLOR="#0033ff" CREATED="1356891078582" ID="ID_1349031937" MODIFIED="1356891083859" POSITION="right" TEXT="SOAP">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1344537798098" ID="ID_1837706383" MODIFIED="1344537807832" TEXT="Emit SOAP call">
+<node COLOR="#00b439" CREATED="1344537792587" ID="ID_433610431" MODIFIED="1356891084872" TEXT="wsimport">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1344537809023" ID="ID_460224606" MODIFIED="1344537821021" TEXT="-Dcom.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump=true">
+<node COLOR="#990000" CREATED="1344537798098" ID="ID_1837706383" MODIFIED="1356891084872" TEXT="Emit SOAP call">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1344537809023" ID="ID_460224606" MODIFIED="1356891084872" TEXT="-Dcom.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump=true">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1356891085774" ID="ID_1383314611" MODIFIED="1356891088459" TEXT="Headers">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1356891089654" ID="ID_1511021089" MODIFIED="1356891089654" TEXT="">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1356891116382" ID="ID_1692719820" MODIFIED="1356891118779" TEXT="BindingProvider">
+<node COLOR="#111111" CREATED="1356891121510" ID="ID_182232991" MODIFIED="1356891125819" TEXT="getBinding()">
+<node COLOR="#111111" CREATED="1356891128766" ID="ID_1608438066" MODIFIED="1356891133411" TEXT="setHeandlerChain()"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1356891371582" ID="ID_1906671897" MODIFIED="1356891378003" TEXT="setOutboundHeaders"/>
 </node>
 </node>
 </node>
