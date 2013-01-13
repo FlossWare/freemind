998fa177333033e48b7c9033943f7c28cc20ad96
Scot P. Floess <sfloess@redhat.com>
2013-01-13 09:33:48 -0500

    Working off lesson 5

--- mr-beer/Mr Beer.mm
+++ mr-beer/Mr Beer.mm
@@ -4,58 +4,6 @@
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1357389226446" ID="ID_490926879" MODIFIED="1357389996440" POSITION="right" TEXT="URLs">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1357389122550" ID="ID_920339814" LINK="http://www.mrbeer.com" MODIFIED="1357389235701" TEXT="Web Site">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1357390584176" ID="ID_513888976" LINK="http://community.mrbeer.com/?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390595825" TEXT="Community">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1357390688728" ID="ID_1154367651" LINK="http://community.mrbeer.com/brewer-s-glossary?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390697168" TEXT="Terms">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1357390716465" ID="ID_1429289800" LINK="http://community.mrbeer.com/frequently-asked-questions?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390726864" TEXT="Knowledge Center">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1357390757729" ID="ID_1516279415" LINK="http://community.mrbeer.com/mrbeer-radio?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390775624" TEXT="Audio">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1357390802985" ID="ID_842193672" LINK="http://community.mrbeer.com/mrbeer-users-group?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390812712" TEXT="User Groups">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1357390827073" ID="ID_1692020948" LINK="http://community.mrbeer.com/forum/index/10-discussion-forum?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390834097" TEXT="Forums">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1357390864033" ID="ID_326584398" LINK="http://www.youtube.com/mrbeer" MODIFIED="1357390877913" TEXT="You Tube">
-<font NAME="SansSerif" SIZE="14"/>
-<node COLOR="#111111" CREATED="1357390996761" ID="ID_1406782762" LINK="http://www.youtube.com/watch?v=gF5kNYHcIBE&amp;list=PL165CB8C41189340D" MODIFIED="1357391010688" TEXT="How To&apos;s"/>
-<node COLOR="#111111" CREATED="1357391388362" ID="ID_167647566" MODIFIED="1357391393784" TEXT="Individual">
-<node COLOR="#111111" CREATED="1357391056937" ID="ID_415064091" LINK="https://www.youtube.com/watch?v=-jG6T6jkyDI&amp;list=PL165CB8C41189340D&amp;index=9" MODIFIED="1357391081508" TEXT="Add fruit to any standard, deluxe, or craft series refill"/>
-<node COLOR="#111111" CREATED="1357391083065" ID="ID_1290535999" LINK="https://www.youtube.com/watch?v=gF5kNYHcIBE&amp;list=PL165CB8C41189340D&amp;index=1" MODIFIED="1357391139162" TEXT="Assemble your swing top bottles"/>
-<node COLOR="#111111" CREATED="1357391093417" ID="ID_1253911113" LINK="https://www.youtube.com/watch?v=b85PoC-z4W4&amp;list=PL165CB8C41189340D&amp;index=2" MODIFIED="1357391166890" TEXT="Attach your stick on thermometer"/>
-<node COLOR="#111111" CREATED="1357391101305" ID="ID_1510451442" LINK="https://www.youtube.com/watch?v=h8Ko7NsdIeU&amp;list=PL165CB8C41189340D&amp;index=3" MODIFIED="1357391186826" TEXT="Use your priming sugar measure"/>
-<node COLOR="#111111" CREATED="1357391105921" ID="ID_590656731" LINK="https://www.youtube.com/watch?v=_gpSpYvIf7Y&amp;list=PL165CB8C41189340D&amp;index=4" MODIFIED="1357391209009" TEXT="Use your bottling wand"/>
-<node COLOR="#111111" CREATED="1357391111008" ID="ID_1637753632" LINK="https://www.youtube.com/watch?v=m_-wJFIfWLg&amp;list=PL165CB8C41189340D&amp;index=6" MODIFIED="1357391229201" TEXT="Use your hydrometer"/>
-<node COLOR="#111111" CREATED="1357391116408" ID="ID_405884158" LINK="https://www.youtube.com/watch?v=fqMxpcIJ67A&amp;list=PL165CB8C41189340D&amp;index=7" MODIFIED="1357391248818" TEXT="Use your hop scale"/>
-<node COLOR="#111111" CREATED="1357391121424" ID="ID_9945532" LINK="https://www.youtube.com/watch?v=URBolj4IQF0&amp;list=PL165CB8C41189340D&amp;index=8" MODIFIED="1357391269258" TEXT="Use the hand bottle capper to cap a beer bottle"/>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1357390914342" ID="ID_229125114" LINK="https://www.facebook.com/mrbeer" MODIFIED="1357390923481" TEXT="Facebook">
-<font NAME="SansSerif" SIZE="14"/>
-</node>
-</node>
-<node COLOR="#00b439" CREATED="1357388952390" ID="ID_1159631405" LINK="http://mailengine.mrbeer.com:81/CT00011508MTc4NjQ=.HTML?D=2013-01-02" MODIFIED="1357390632550" TEXT="Brewing Instructions Video">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1357389570886" ID="ID_725167080" LINK="https://www.mrbeer.com/category-exec/category_id/99?utm_source=BrewU2&amp;utm_medium=BrewU2&amp;utm_campaign=BrewU2" MODIFIED="1357390630558" TEXT=" Craftmeister Oxygen Brewery Wash">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-</node>
 <node COLOR="#0033ff" CREATED="1357389006942" ID="ID_1010086104" MODIFIED="1357389991287" POSITION="right" TEXT="To brew">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
@@ -87,6 +35,111 @@
 <node COLOR="#0033ff" CREATED="1357389945813" ID="ID_7963177" LINK="Lessons.mm" MODIFIED="1357389945815" POSITION="right" TEXT="Lessons">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1358086223645" ID="ID_811967682" MODIFIED="1358086224645" POSITION="right" TEXT="Terms">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1358086225853" ID="ID_810091393" MODIFIED="1358086227198" TEXT="HME">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1358086228093" ID="ID_1261409421" MODIFIED="1358086238244" TEXT="Hop Malt Extract">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1358086345109" ID="ID_1408856920" MODIFIED="1358086350820" TEXT="14 different styles">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1358086362997" ID="ID_319641565" MODIFIED="1358086373356" TEXT="American and Internations"/>
+<node COLOR="#111111" CREATED="1358086373733" ID="ID_299522990" MODIFIED="1358086375804" TEXT="10 flavors"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1358086455605" ID="ID_75404959" MODIFIED="1358086456708" TEXT="LME">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1358086457509" ID="ID_1452673856" MODIFIED="1358086464669" TEXT="Liquid Malt Extract">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1358087071589" ID="ID_1668972018" MODIFIED="1358087073165" TEXT="DME">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1358087073997" ID="ID_1181720673" MODIFIED="1358087077141" TEXT="Dry Malt Extract">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1358086892461" ID="ID_1960518341" MODIFIED="1358086893621" TEXT="IBU">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1358086895597" ID="ID_1383842570" MODIFIED="1358086901269" TEXT="International Bittering Units">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1358087389293" ID="ID_56261461" MODIFIED="1358087391702" TEXT="Malt sugars">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1358087401165" ID="ID_1689605148" MODIFIED="1358087415365" TEXT="HME">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1358087403581" ID="ID_712902277" MODIFIED="1358087413350" TEXT="LME">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1358087408445" ID="ID_1862505178" MODIFIED="1358087410708" TEXT="DME">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1357389226446" ID="ID_490926879" MODIFIED="1357389996440" POSITION="left" TEXT="URLs">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1357389122550" ID="ID_920339814" LINK="http://www.mrbeer.com" MODIFIED="1357389235701" TEXT="Web Site">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1357390584176" ID="ID_513888976" LINK="http://community.mrbeer.com/?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1357390595825" TEXT="Community">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1357390757729" ID="ID_1516279415" LINK="http://community.mrbeer.com/mrbeer-radio?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1358086937391" TEXT="Audio">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390914342" ID="ID_229125114" LINK="https://www.facebook.com/mrbeer" MODIFIED="1358086973790" TEXT="Facebook">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390827073" ID="ID_1692020948" LINK="http://community.mrbeer.com/forum/index/10-discussion-forum?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1358086941709" TEXT="Forums">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390716465" ID="ID_1429289800" LINK="http://community.mrbeer.com/frequently-asked-questions?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1358086995991" TEXT="Knowledge Center">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1358087004365" ID="ID_301365659" LINK="https://www.mrbeer.com/category-exec/category_id/14" MODIFIED="1358087014564" TEXT="Recipes">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390688728" ID="ID_1154367651" LINK="http://community.mrbeer.com/brewer-s-glossary?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1358086939681" TEXT="Terms">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390802985" ID="ID_842193672" LINK="http://community.mrbeer.com/mrbeer-users-group?utm_source=BrewU3&amp;utm_medium=BrewU3&amp;utm_campaign=BrewU3" MODIFIED="1358086946823" TEXT="User Groups">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1357390864033" ID="ID_326584398" LINK="http://www.youtube.com/mrbeer" MODIFIED="1357390877913" TEXT="You Tube">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1357390996761" ID="ID_1406782762" LINK="http://www.youtube.com/watch?v=gF5kNYHcIBE&amp;list=PL165CB8C41189340D" MODIFIED="1357391010688" TEXT="How To&apos;s"/>
+<node COLOR="#111111" CREATED="1357391388362" ID="ID_167647566" MODIFIED="1357391393784" TEXT="Individual">
+<node COLOR="#111111" CREATED="1357391056937" ID="ID_415064091" LINK="https://www.youtube.com/watch?v=-jG6T6jkyDI&amp;list=PL165CB8C41189340D&amp;index=9" MODIFIED="1357391081508" TEXT="Add fruit to any standard, deluxe, or craft series refill"/>
+<node COLOR="#111111" CREATED="1357391083065" ID="ID_1290535999" LINK="https://www.youtube.com/watch?v=gF5kNYHcIBE&amp;list=PL165CB8C41189340D&amp;index=1" MODIFIED="1357391139162" TEXT="Assemble your swing top bottles"/>
+<node COLOR="#111111" CREATED="1357391093417" ID="ID_1253911113" LINK="https://www.youtube.com/watch?v=b85PoC-z4W4&amp;list=PL165CB8C41189340D&amp;index=2" MODIFIED="1357391166890" TEXT="Attach your stick on thermometer"/>
+<node COLOR="#111111" CREATED="1357391101305" ID="ID_1510451442" LINK="https://www.youtube.com/watch?v=h8Ko7NsdIeU&amp;list=PL165CB8C41189340D&amp;index=3" MODIFIED="1357391186826" TEXT="Use your priming sugar measure"/>
+<node COLOR="#111111" CREATED="1357391105921" ID="ID_590656731" LINK="https://www.youtube.com/watch?v=_gpSpYvIf7Y&amp;list=PL165CB8C41189340D&amp;index=4" MODIFIED="1357391209009" TEXT="Use your bottling wand"/>
+<node COLOR="#111111" CREATED="1357391111008" ID="ID_1637753632" LINK="https://www.youtube.com/watch?v=m_-wJFIfWLg&amp;list=PL165CB8C41189340D&amp;index=6" MODIFIED="1357391229201" TEXT="Use your hydrometer"/>
+<node COLOR="#111111" CREATED="1357391116408" ID="ID_405884158" LINK="https://www.youtube.com/watch?v=fqMxpcIJ67A&amp;list=PL165CB8C41189340D&amp;index=7" MODIFIED="1357391248818" TEXT="Use your hop scale"/>
+<node COLOR="#111111" CREATED="1357391121424" ID="ID_9945532" LINK="https://www.youtube.com/watch?v=URBolj4IQF0&amp;list=PL165CB8C41189340D&amp;index=8" MODIFIED="1357391269258" TEXT="Use the hand bottle capper to cap a beer bottle"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1357388952390" ID="ID_1159631405" LINK="http://mailengine.mrbeer.com:81/CT00011508MTc4NjQ=.HTML?D=2013-01-02" MODIFIED="1357390632550" TEXT="Brewing Instructions Video">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1357389570886" ID="ID_725167080" LINK="https://www.mrbeer.com/category-exec/category_id/99?utm_source=BrewU2&amp;utm_medium=BrewU2&amp;utm_campaign=BrewU2" MODIFIED="1357390630558" TEXT=" Craftmeister Oxygen Brewery Wash">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
 </node>
 </node>
 </map>
