cb4f09bff4e4cc9e073890de97bfb13d01178aeb
Scot P. Floess <sfloess@redhat.com>
2014-11-16 19:56:26 -0500

    More vagrant related mappings

--- system-administration/Oh My Vagrant.mm
+++ system-administration/Oh My Vagrant.mm
@@ -4,5 +4,16 @@
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1416185489290" ID="ID_507114216" MODIFIED="1416185493938" POSITION="right" TEXT="Network">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1416185495550" ID="ID_1579364048" MODIFIED="1416185501518" TEXT="vagrant.yaml">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1416185504086" ID="ID_750487544" MODIFIED="1416185514614" TEXT="network / namespace should match up">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
 </node>
 </map>
