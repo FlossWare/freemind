2d5ce821e7f15d5ac8a9c180c3ce7cb9ea1d441d
Scot P. Floess <sfloess@redhat.com>
2015-10-31 14:17:56 -0400

    Added setup and rhc command mappings for Open Shift

--- development/Open Shift.mm
+++ development/Open Shift.mm
@@ -1,9 +1,20 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#000000" CREATED="1364908128833" ID="ID_543915835" LINK="Development.mm" MODIFIED="1364908142552" TEXT="Open Shift">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1446315366984" ID="ID_1133378785" MODIFIED="1446315368343" POSITION="right" TEXT="rhc">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446315381538" ID="ID_1733250623" MODIFIED="1446315384439" TEXT="List apps">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446315385636" ID="ID_679749191" MODIFIED="1446315387486" TEXT="rhc apps">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
 <node COLOR="#0033ff" CREATED="1387906970829" ID="ID_603656898" MODIFIED="1387906976075" POSITION="right" TEXT="Jenkins Cartridge">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
@@ -32,6 +43,38 @@
 <node COLOR="#111111" CREATED="1387907142912" ID="ID_750160117" MODIFIED="1387907145075" TEXT="value">
 <node COLOR="#111111" CREATED="1387907146673" ID="ID_439755444" MODIFIED="1387907152175" TEXT="-Xmx512m -XX:MaxPermSize=512m"/>
 </node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1446315233141" ID="ID_1425817555" MODIFIED="1446315235942" POSITION="left" TEXT="Setup">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446315238063" ID="ID_1238389492" MODIFIED="1446315241987" TEXT="Channels">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446315251047" ID="ID_1695033009" MODIFIED="1446315251881" TEXT="sudo yum-config-manager --enable [architecture][operating_system]-optional-6">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446315260060" ID="ID_735828344" MODIFIED="1446315261201" TEXT="sudo rhn-channel --add --channel=[architecture]-[operating_system]-optional-6">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1446315275210" ID="ID_776665409" MODIFIED="1446315275990" TEXT="sudo rhn-channel --add --channel=rhel-x86_64-workstation-optional-6"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1446315298519" ID="ID_1762644984" MODIFIED="1446315302642" TEXT="Tooling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446315306325" ID="ID_41502082" MODIFIED="1446315307105" TEXT="sudo gem install rhc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446315314672" ID="ID_938199303" MODIFIED="1446315315489" TEXT="sudo gem update rhc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1446315317309" ID="ID_872581869" MODIFIED="1446315327454" TEXT="Machine">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446315337990" ID="ID_1122374565" MODIFIED="1446315337991" TEXT="rhc setup">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
 </node>
