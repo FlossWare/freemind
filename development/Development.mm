f4c9ebfdc921fa978fc28ab3b88a13e7736761b6
Scot P. Floess <sfloess@redhat.com>
2016-01-22 16:48:04 -0500

    Moved Development mapping around to see better

--- development/Development.mm
+++ development/Development.mm
@@ -4,38 +4,6 @@
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1390752245771" ID="ID_499215218" MODIFIED="1390752248035" POSITION="right" TEXT="IDE">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1292966745156" ID="ID_1485368427" LINK="Eclipse.mm" MODIFIED="1390752255412" TEXT="Eclipse">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1295700145946" ID="ID_24812944" LINK="Netbeans.mm" MODIFIED="1390752255415" TEXT="Netbeans">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1295700074922" ID="ID_548272597" LINK="JEE.mm" MODIFIED="1390752372902" POSITION="right" TEXT="JEE">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1295699991417" ID="ID_1365467272" LINK="Glassfish.mm" MODIFIED="1390752318489" TEXT="Glassfish">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1333546073601" ID="ID_1392989418" LINK="JBoss.mm" MODIFIED="1390752318494" TEXT="JBoss">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1390752350973" ID="ID_263591765" MODIFIED="1390752358712" POSITION="right" TEXT="Database">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1363289420757" ID="ID_1931664902" LINK="MySQL.mm" MODIFIED="1390752354872" TEXT="MySQL">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-</node>
 <node COLOR="#0033ff" CREATED="1390752301974" ID="ID_1317670251" MODIFIED="1390752303348" POSITION="right" TEXT="Language">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
@@ -127,6 +95,38 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 <node COLOR="#00b439" CREATED="1293055220871" ID="ID_249445657" LINK="VIM.mm" MODIFIED="1390752342634" TEXT="VIM">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1390752245771" ID="ID_499215218" MODIFIED="1390752248035" POSITION="left" TEXT="IDE">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1292966745156" ID="ID_1485368427" LINK="Eclipse.mm" MODIFIED="1390752255412" TEXT="Eclipse">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1295700145946" ID="ID_24812944" LINK="Netbeans.mm" MODIFIED="1390752255415" TEXT="Netbeans">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1295700074922" ID="ID_548272597" LINK="JEE.mm" MODIFIED="1390752372902" POSITION="left" TEXT="JEE">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1295699991417" ID="ID_1365467272" LINK="Glassfish.mm" MODIFIED="1390752318489" TEXT="Glassfish">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1333546073601" ID="ID_1392989418" LINK="JBoss.mm" MODIFIED="1390752318494" TEXT="JBoss">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1390752350973" ID="ID_263591765" MODIFIED="1390752358712" POSITION="left" TEXT="Database">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1363289420757" ID="ID_1931664902" LINK="MySQL.mm" MODIFIED="1390752354872" TEXT="MySQL">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
