2441d2457db12fa8e4fb027905925e95a064aab0
Scot P. Floess <sfloess@redhat.com>
2018-12-13 14:51:53 -0500

    Latest

--- system-administration/Hardware.mm
+++ system-administration/Hardware.mm
@@ -4,12 +4,92 @@
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1327003827279" ID="ID_1488479797" LINK="DNS-320.mm" MODIFIED="1543855036079" POSITION="right" TEXT="DNS-320">
+<node COLOR="#0033ff" CREATED="1509824381608" ID="ID_444956781" LINK="https://www.dell.com/support/home/us/en/04" MODIFIED="1544729693472" POSITION="right" TEXT="Dell">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1509824794869" ID="ID_758600074" LINK="Alt-F.mm" MODIFIED="1543855036080" TEXT="Alt-F">
+<node COLOR="#00b439" CREATED="1509824395388" ID="ID_69158302" MODIFIED="1543854570170" TEXT="Precision">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1543949845606" ID="ID_649429232" MODIFIED="1543949854243" TEXT="BIOS Updates">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1543949878997" ID="ID_284609430" MODIFIED="1543949884281" TEXT="BIN file">
+<node COLOR="#111111" CREATED="1543949884917" ID="ID_882211361" MODIFIED="1543949905561">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tail <b><i>-n</i></b>&#160;+`awk '/^__ARC__/ { print NR + 1; exit 0; }' $0` $0 | gzip -cd &gt;$_PRG
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1458647046393" ID="ID_220428500" LINK="https://www.dell.com/support/home/us/en/04/product-support/product/precision-470/diagnose" MODIFIED="1543949753633" TEXT="470">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1543877392965" ID="ID_1164101741" LINK="http://www.manualslib.com/manual/36929/Dell-Precision-Workstation-470.html?page=35#manual" MODIFIED="1543877422475" TEXT="Error Codes">
+<node COLOR="#111111" CREATED="1458740267008" ID="ID_284821191" MODIFIED="1509824386469" TEXT="Lights">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1458647033377" ID="ID_1086639960" MODIFIED="1458740271730" TEXT="B&amp;D yellow">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1458647041497" ID="ID_319787348" MODIFIED="1458740271731" TEXT="A&amp;C green">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1458740283828" ID="ID_66794013" MODIFIED="1509824386470" TEXT="Memory modules are detected, but a memory configuration or compatibility error exists">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1458740296063" ID="ID_117751785" MODIFIED="1509824386470" TEXT="&#x2022; Ensure that no special memory module requirements exist (see &quot;Memory Overview&quot; in the User&apos;s Guide).&#xa;&#x2022; Verify that the memory modules that you are installing are compatible with your computer.&#xa;&#x2022; Reinstall the memory modules and restart the computer. &#x2022; If the problem persists, contact Del">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1543877364822" ID="ID_1363971372" LINK="https://www.dell.com/support/home/us/en/19/product-support/product/precision-470/drivers" MODIFIED="1543877376272" TEXT="Drivers">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1477655169117" ID="ID_1773135455" LINK="https://www.dell.com/support/home/us/en/04/product-support/product/precision-490-dt/diagnose" MODIFIED="1543949792763" TEXT="490">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1544729119900" ID="ID_1759007977" LINK="https://www.dell.com/support/home/us/en/04/product-support/product/precision-490-dt/drivers" MODIFIED="1544729130258" TEXT="Drivers"/>
+</node>
+<node COLOR="#990000" CREATED="1512060771416" ID="ID_1472742335" LINK="https://www.dell.com/support/home/us/en/04/product-support/product/precision-t5400/diagnose" MODIFIED="1543949987757" TEXT="T5400">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1544729139544" ID="ID_1423835909" LINK="https://www.dell.com/support/home/us/en/04/product-support/product/precision-t5400/drivers" MODIFIED="1544729149657" TEXT="Drivers"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1534278295572" ID="ID_732158883" MODIFIED="1543854570171" TEXT="Inspiron">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1534278302928" ID="ID_205108321" MODIFIED="1544729660263" TEXT="i5577-7700BLK">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1544729848876" ID="ID_240138594" LINK="https://downloads.dell.com/Manuals/all-products/esuprt_laptop/esuprt_inspiron_laptop/inspiron-mini1012_service%20manual_en-us.pdf" MODIFIED="1544729863539" TEXT="Service Manual"/>
+<node COLOR="#111111" CREATED="1544729875136" ID="ID_190753027" LINK="https://www.dell.com/support/manuals/us/en/19/inspiron-15-5577-gaming-laptop/inspiron15_5577_setupandspecs/specifications?guid=guid-444129f4-3593-4202-9dc5-193ac5e31c25&amp;lang=en-us" MODIFIED="1544729891588" TEXT="Specifications">
+<node COLOR="#111111" CREATED="1534278363277" ID="ID_578997617" MODIFIED="1534278365406" TEXT="Dell G5587-7866BLK-PUS G5 15 5587 Gaming Laptop 15.6&quot; LED Display, 8th Gen Intel i7 Processor, 16GB Memory, 128GB SSD+1TB HDD, NVIDIA GeForce GTX 1050Ti, Licorice Black "/>
+<node COLOR="#111111" CREATED="1534284082901" ID="ID_1166126981" MODIFIED="1534284085825" TEXT="i7-7700hq"/>
+</node>
+<node COLOR="#111111" CREATED="1544729969516" ID="ID_343447291" LINK="https://www.dell.com/support/manuals/us/en/19/inspiron-15-5577-gaming-laptop/inspiron15_5577_setupandspecs/memory?guid=guid-75c3ed1c-1bb5-4c43-854d-648447036c7b&amp;lang=en-us" MODIFIED="1544729979971" TEXT="Memory">
+<node COLOR="#111111" CREATED="1544729997087" ID="ID_1494045074" LINK="https://www.amazon.com/ddr4-2400-mhz-ram/s?page=1&amp;rh=i%3Aaps%2Ck%3Addr4%202400%20mhz%20ram" MODIFIED="1544730004325" TEXT="Amazon"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1544729087535" ID="ID_1363252769" MODIFIED="1544729097704" POSITION="right" TEXT="D-Link">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1327003827279" ID="ID_1488479797" LINK="DNS-320.mm" MODIFIED="1544729099514" TEXT="DNS-320">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1509824794869" ID="ID_758600074" LINK="Alt-F.mm" MODIFIED="1544729099515" TEXT="Alt-F">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1542639159736" ID="ID_471738460" MODIFIED="1543855036080" POSITION="right" TEXT="Lenovo">
@@ -47,7 +127,7 @@
 <node COLOR="#0033ff" CREATED="1514664686315" ID="ID_88985905" MODIFIED="1543855036082" POSITION="right" TEXT="Toshiba">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1514664688683" FOLDED="true" ID="ID_1304119867" LINK="https://support.toshiba.com/support/modelHome?freeText=3478693" MODIFIED="1543855036083" TEXT="Satellite s855-s5378">
+<node COLOR="#00b439" CREATED="1514664688683" ID="ID_1304119867" LINK="https://support.toshiba.com/support/modelHome?freeText=3478693" MODIFIED="1544729079517" TEXT="Satellite s855-s5378">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1514664702399" ID="ID_1141259057" LINK="https://www.justanswer.com/laptop/8528e-toshiba-satellite-s855-s5378-laptop-windows.html" MODIFIED="1543855036083" TEXT="Boot keys">
