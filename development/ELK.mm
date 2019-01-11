ee6314f744c24dac03cd9d575832c4e4606d334a
Scot P. Floess <sfloess@redhat.com>
2019-01-11 16:07:58 -0500

    Latest

--- development/ELK.mm
+++ development/ELK.mm
@@ -16,9 +16,34 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1547148127980" ID="ID_681256125" MODIFIED="1547148202706" POSITION="right" TEXT="Use OpenJDK 9">
+<node COLOR="#0033ff" CREATED="1547229199357" ID="ID_1391984565" MODIFIED="1547229202005" POSITION="right" TEXT="OS">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1547229205281" ID="ID_1501649268" MODIFIED="1547229206749" TEXT="Debian">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1547229194734" ID="ID_11008423" MODIFIED="1547229210407" TEXT="arm">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1547229764724" ID="ID_566124023" MODIFIED="1547229771788" TEXT="apt-get install">
+<node COLOR="#111111" CREATED="1547229777120" ID="ID_1140040938" MODIFIED="1547229779326" TEXT="openjdk-9-jdk"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1547229211538" ID="ID_1826057179" MODIFIED="1547229213249" TEXT="CentOS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1547229213882" ID="ID_941109679" MODIFIED="1547229220429" TEXT="yum install">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1547229222242" ID="ID_1147431752" MODIFIED="1547229226477" TEXT="java-1.8.0-openjdk-devel"/>
+<node COLOR="#111111" CREATED="1547229622994" ID="ID_1932676936" MODIFIED="1547229624838" TEXT="epel-release"/>
+<node COLOR="#111111" CREATED="1547229601621" ID="ID_782377284" MODIFIED="1547229604078" TEXT="wget"/>
+<node COLOR="#111111" CREATED="1547229604900" ID="ID_1368376838" MODIFIED="1547229608229" TEXT="rsync"/>
+<node COLOR="#111111" CREATED="1547230024364" ID="ID_416282913" MODIFIED="1547230026226" TEXT="vim-enhanced"/>
+<node COLOR="#111111" CREATED="1547233219461" ID="ID_439950111" MODIFIED="1547233220409" TEXT="htop"/>
+<node COLOR="#111111" CREATED="1547233220821" ID="ID_1142412847" MODIFIED="1547233224309" TEXT="net-tools"/>
+</node>
+</node>
 </node>
 <node COLOR="#0033ff" CREATED="1547148173187" ID="ID_1738602490" MODIFIED="1547148176072" POSITION="right" TEXT="How To">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
@@ -32,6 +57,7 @@
 <node COLOR="#111111" CREATED="1546974580550" ID="ID_1078684546" LINK="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.4.deb" MODIFIED="1546976534029" TEXT="deb">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
+<node COLOR="#111111" CREATED="1547229414977" ID="ID_1626395221" LINK="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.4.rpm" MODIFIED="1547229424665" TEXT="rpm"/>
 </node>
 <node COLOR="#990000" CREATED="1546975946929" ID="ID_1513726800" MODIFIED="1547148193953" TEXT="configure">
 <edge STYLE="bezier" WIDTH="thin"/>
@@ -83,10 +109,12 @@
 <node COLOR="#111111" CREATED="1546974518037" ID="ID_1985088402" LINK="https://artifacts.elastic.co/downloads/logstash/logstash-6.5.4.deb" MODIFIED="1546976534040" TEXT="deb">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
+<node COLOR="#111111" CREATED="1547229427373" ID="ID_1297471886" LINK="https://artifacts.elastic.co/downloads/logstash/logstash-6.5.4.rpm" MODIFIED="1547229455201" TEXT="rpm"/>
 </node>
 <node COLOR="#990000" CREATED="1547149714141" ID="ID_1958854447" LINK="https://www.elastic.co/guide/en/logstash/current/configuration.html" MODIFIED="1547149725081" TEXT="configure">
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1547149683978" ID="ID_917341494" MODIFIED="1547149686136" TEXT="bin/logstash -f logstash-simple.conf"/>
+<node COLOR="#111111" CREATED="1547237553149" ID="ID_500662760" MODIFIED="1547237555740" TEXT="arm">
 <node COLOR="#111111" CREATED="1547149815680" ID="ID_351902465" MODIFIED="1547149817938" TEXT="/etc/logstash/jvm.options">
 <node COLOR="#111111" CREATED="1546976043761" ID="ID_506938660" MODIFIED="1546976168711">
 <richcontent TYPE="NODE"><html>
@@ -105,6 +133,7 @@
 </node>
 </node>
 </node>
+</node>
 <node COLOR="#990000" CREATED="1547149677493" ID="ID_1928964299" MODIFIED="1547149736319" TEXT="run">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
@@ -118,6 +147,7 @@
 <node COLOR="#111111" CREATED="1546974665530" ID="ID_1999774365" LINK="https://artifacts.elastic.co/downloads/kibana/kibana-6.5.4-amd64.deb" MODIFIED="1546976534042" TEXT="deb">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
+<node COLOR="#111111" CREATED="1547229430141" ID="ID_940215001" LINK="https://artifacts.elastic.co/downloads/kibana/kibana-6.5.4-x86_64.rpm" MODIFIED="1547229475398" TEXT="rpm"/>
 <node COLOR="#111111" CREATED="1546974750378" ID="ID_80661411" MODIFIED="1546982481143" TEXT="dpkg -x [kibana deb file] /">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
@@ -136,6 +166,41 @@
 <node COLOR="#0033ff" CREATED="1546977419469" ID="ID_54454236" LINK="https://logz.io/blog/elk-stack-raspberry-pi/" MODIFIED="1547148217985" POSITION="right" TEXT="logs.io">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1547233984656" ID="ID_1059457135" MODIFIED="1547233995733" POSITION="right" TEXT="Digital Ocean">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1547233996257" ID="ID_689894171" LINK="https://www.digitalocean.com/community/tutorials/how-to-centralize-logs-with-rsyslog-logstash-and-elasticsearch-on-ubuntu-14-04" MODIFIED="1547234007882" TEXT="Setup Stack">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1547239712616" ID="ID_702491999" MODIFIED="1547239714464" POSITION="right" TEXT="rsyslog">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1547239715356" ID="ID_650961313" MODIFIED="1547239718240" TEXT="custom log files">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1547239773172" ID="ID_1210380954" MODIFIED="1547239774399" TEXT="perms">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1547239725956" ID="ID_102634143" MODIFIED="1547239777197" TEXT="owner">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1547239728848" ID="ID_1567847907" MODIFIED="1547239731176" TEXT="syslog"/>
+</node>
+<node COLOR="#111111" CREATED="1547239732695" ID="ID_1423053902" MODIFIED="1547239777200" TEXT="group">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1547239734524" ID="ID_1175501673" MODIFIED="1547239735536" TEXT="adm"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1547239778424" ID="ID_856587033" MODIFIED="1547239780976" TEXT="/etc/rsyslog.conf">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1547239785044" ID="ID_712009864" MODIFIED="1547239819245" TEXT="local7.*  /var/log/anm.log "/>
+</node>
+<node COLOR="#990000" CREATED="1547239795260" ID="ID_673993406" MODIFIED="1547239797295" TEXT="/etc/rsyslog.d/50-default.conf ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1547239807400" ID="ID_1561170661" MODIFIED="1547239823676" TEXT="local7.*  /var/log/anm.log"/>
+</node>
+</node>
 </node>
 </node>
 </map>
