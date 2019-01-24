748fe9f6f3c71de4d50fb7acea06356d57fb412d
Scot P. Floess <sfloess@redhat.com>
2019-01-24 16:05:20 -0500

    Latest

--- development/Archiva.mm
+++ development/Archiva.mm
@@ -12,6 +12,41 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
+<node COLOR="#0033ff" CREATED="1548341141952" ID="ID_614218184" LINK="https://ti57.blogspot.com/2013/05/apache-archiva-on-raspberry-pi.html" MODIFIED="1548341191568" POSITION="right" TEXT="arm">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1548341162979" ID="ID_191579138" LINK="https://wrapper.tanukisoftware.com/doc/english/download.jsp" MODIFIED="1548341410552" TEXT="Download java service wrapper">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#00b439" CREATED="1548341260008" ID="ID_1378242796" MODIFIED="1548341413551" TEXT="Unpack java service wrapper">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node COLOR="#00b439" CREATED="1548341268123" ID="ID_727297832" MODIFIED="1548341419494" TEXT="cp to $ARCHIVA_HOME">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#990000" CREATED="1548341324200" ID="ID_1492552882" MODIFIED="1548341422970" TEXT="lib">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-4"/>
+<node COLOR="#111111" CREATED="1548341296598" ID="ID_1668590533" MODIFIED="1548341331361" TEXT="wrapper.jar">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1548341303613" ID="ID_854704615" MODIFIED="1548341331364" TEXT="libwrapper.so">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1548341340817" ID="ID_1542765606" MODIFIED="1548341425736" TEXT="bin">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-5"/>
+<node COLOR="#111111" CREATED="1548341349561" ID="ID_1773912094" MODIFIED="1548341442304" TEXT="$WRAPPER_HOME/bin/wrapper as wrapper-linux-armv7l-32"/>
+</node>
+</node>
+</node>
 <node COLOR="#0033ff" CREATED="1548261360664" ID="ID_768135298" MODIFIED="1548261363026" POSITION="right" TEXT="How To">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
