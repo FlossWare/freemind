ab53d3d89db9805157b0393e5292e37dc8e0812c
Scot P. floess <sfloess@nc.rr.com>
2019-11-05 09:18:23 -0500

    Latest

--- system-administration/virt-manager.mm
+++ system-administration/virt-manager.mm
@@ -11,6 +11,13 @@
 <node COLOR="#0033ff" CREATED="1546370067856" ID="ID_1465859679" MODIFIED="1546370069380" POSITION="right" TEXT="How To">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1572719208745" ID="ID_127347206" MODIFIED="1572719212014" TEXT="Enable sound">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1572719214056" ID="ID_1922098571" MODIFIED="1572719222627" TEXT="Model:  AC97">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
 </node>
 </node>
 </map>
