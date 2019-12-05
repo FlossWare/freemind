74ac0ac794d5c866d0b3c7086266460a85254505
Scot P. floess <sfloess@nc.rr.com>
2019-12-05 16:50:11 -0500

    Latest

--- Reference.mm
+++ Reference.mm
@@ -3,7 +3,7 @@
 <node COLOR="#000000" CREATED="1288808822794" ID="ID_1348475184" MODIFIED="1471099751568" TEXT="Reference">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1290436598971" ID="ID_1279390877" LINK="development/Development.mm" MODIFIED="1548258601691" POSITION="right" TEXT="Development">
+<node COLOR="#0033ff" CREATED="1290436598971" ID="ID_1279390877" LINK="development/Development.mm" MODIFIED="1575477846208" POSITION="right" TEXT="Development">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
@@ -50,7 +50,7 @@
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1360104143237" ID="ID_1837158144" LINK="mathematica/Mathematica.mm" MODIFIED="1542471125744" TEXT="Mathematica">
+<node COLOR="#00b439" CREATED="1360104143237" ID="ID_1837158144" LINK="mathematica/Mathematica.mm" MODIFIED="1575472885855" TEXT="Mathematica">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
--- system-administration/Firefox.mm
+++ system-administration/Firefox.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1430479541378" ID="ID_447882735" LINK="System%20Administration.mm" MODIFIED="1430479577270" TEXT="Firefox">
+<node COLOR="#000000" CREATED="1430479541378" ID="ID_447882735" LINK="System%20Administration.mm" MODIFIED="1575582047227" TEXT="Firefox">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
