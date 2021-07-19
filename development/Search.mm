393f8424fed2c2d3685396fe2cc962f1cbdd7401
Flossy <sfloess@redhat.com>
2021-07-19 16:24:50 -0400

    WIP - Latest

--- development/Search.mm
+++ development/Search.mm
@@ -4,10 +4,13 @@
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1471002966446" ID="ID_114755864" MODIFIED="1471002967957" POSITION="right" TEXT="PER">
+<node COLOR="#0033ff" CREATED="1626697914434" ID="ID_812305380" MODIFIED="1626697915822" POSITION="right" TEXT="Terms">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1471002975426" ID="ID_1748422150" MODIFIED="1554211228498">
+<node COLOR="#00b439" CREATED="1471002966446" ID="ID_114755864" MODIFIED="1626697918134" TEXT="PER">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1471002975426" ID="ID_1748422150" MODIFIED="1626697918135">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -17,16 +20,15 @@
       <b><i>P</i></b>ages <b><i>E</i></b>xits <b><i>R</i></b>efinements
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554210914082" ID="ID_1848250457" MODIFIED="1626697919951" TEXT="MRR">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1554210914082" ID="ID_1848250457" MODIFIED="1554211154392" POSITION="right" TEXT="MRR">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1554210917699" ID="ID_1563840083" MODIFIED="1554211188275">
+<node COLOR="#990000" CREATED="1554210917699" ID="ID_1563840083" MODIFIED="1626697919953">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -36,11 +38,52 @@
       <b><i>M</i></b>ean <b><i>R</i></b>eciprical <b><i>R</i></b>ank
     </p>
   </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1626697394809" ID="ID_489225682" MODIFIED="1626697921460" TEXT="SERP">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1626697398068" ID="ID_1450160638" MODIFIED="1626697921461">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>S</i></b>earch <b><i>E</i></b>ngine <b><i>R</i></b>esults <b><i>P</i></b>ages
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1626697452080" ID="ID_1980621326" MODIFIED="1626697922853" TEXT="SEO">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1626697455363" ID="ID_1060905841" MODIFIED="1626697942266">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>S</i></b>earch <b><i>E</i></b>ngine <b><i>O</i></b>ptimization
+    </p>
+  </body>
 </html>
 </richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
+<font NAME="SansSerif" SIZE="14"/>
 </node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1626697692252" ID="ID_985983001" LINK="https://backlinko.com/hub/seo" MODIFIED="1626697707336" POSITION="right" TEXT="SEO Marketing Hub">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
--- system-administration/Amazon.mm
+++ system-administration/Amazon.mm
@@ -167,22 +167,68 @@
 <node COLOR="#0033ff" CREATED="1626378104391" ID="ID_1519376655" MODIFIED="1626378106859" POSITION="right" TEXT="AWS">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1626378108229" ID="ID_1986991416" MODIFIED="1626378112476" TEXT="IaC">
+<node COLOR="#00b439" CREATED="1626724007885" ID="ID_302136975" MODIFIED="1626724015141" TEXT="Reading">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1626378116151" ID="ID_1839439847" MODIFIED="1626379427482" TEXT="Infrastructure as Code">
+<node COLOR="#990000" CREATED="1626723992684" ID="ID_669110390" LINK="https://aws.amazon.com/getting-started/fundamentals-core-concepts/?e=gs2020&amp;p=gsrc" MODIFIED="1626724113301" TEXT="Core Concepts">
+<edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1626725162825" ID="ID_1837140103" MODIFIED="1626725165934" TEXT="Features">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1626725170385" ID="ID_894910075" MODIFIED="1626725174742" TEXT="CloudWatch">
+<node COLOR="#111111" CREATED="1626725175979" ID="ID_359104832" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/WhatIsCloudWatchLogs.html" MODIFIED="1626725302702" TEXT="Logs"/>
+<node COLOR="#111111" CREATED="1626725179027" ID="ID_440240857" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/AnalyzingLogData.html" MODIFIED="1626725293086" TEXT="Logs Insight"/>
+<node COLOR="#111111" CREATED="1626725181937" ID="ID_67597146" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch_Dashboards.html" MODIFIED="1626725282586" TEXT="Dashboards"/>
+<node COLOR="#111111" CREATED="1626725184827" ID="ID_1017897382" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html" MODIFIED="1626725271240" TEXT="Alarms"/>
+<node COLOR="#111111" CREATED="1626725186571" ID="ID_458153075" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/publishingMetrics.html" MODIFIED="1626725314910" TEXT="Custom Metrics"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1626724109551" ID="ID_101110403" MODIFIED="1626724112201" TEXT="Services">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1626724056773" ID="ID_1136946865" LINK="https://docs.aws.amazon.com/cloudformation/?id=docs_gateway" MODIFIED="1626724114839" TEXT="CloudFormation">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1626378144765" ID="ID_1287156383" LINK="https://docs.aws.amazon.com/cdk/?id=docs_gateway" MODIFIED="1626724174695" TEXT="Cloud Development Kit (CDK)">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1626379493699" ID="ID_95428678" LINK="https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html" MODIFIED="1626379530116" TEXT="API Reference"/>
+</node>
+<node COLOR="#111111" CREATED="1626725336970" ID="ID_213103459" LINK="https://aws.amazon.com/athena/?id=docs_gateway" MODIFIED="1626725371242" TEXT="Athena"/>
+<node COLOR="#111111" CREATED="1626725339115" ID="ID_899631204" LINK="https://docs.aws.amazon.com/cloudwatch/?id=docs_gateway" MODIFIED="1626725380821" TEXT="CloudWatch"/>
+<node COLOR="#111111" CREATED="1626725343538" ID="ID_1768433123" LINK="https://docs.aws.amazon.com/xray/?id=docs_gateway" MODIFIED="1626725393744" TEXT="X-Ray"/>
+<node COLOR="#111111" CREATED="1626725346258" ID="ID_364455509" LINK="https://docs.aws.amazon.com/cloudtrail/?id=docs_gateway" MODIFIED="1626725410109" TEXT="CloudTrail"/>
+<node COLOR="#111111" CREATED="1626725349331" ID="ID_1865002440" LINK="https://docs.aws.amazon.com/elasticsearch-service/?id=docs_gateway" MODIFIED="1626725420564" TEXT="ElasticSearch Service"/>
+<node COLOR="#111111" CREATED="1626725354194" ID="ID_1395584043" LINK="https://docs.aws.amazon.com/rds/?id=docs_gateway" MODIFIED="1626725434702" TEXT="RDS"/>
+<node COLOR="#111111" CREATED="1626725357138" ID="ID_236252477" LINK="https://docs.aws.amazon.com/cloudtrail/?id=docs_gateway" MODIFIED="1626725444638" TEXT="CloudTrail"/>
+</node>
+<node COLOR="#990000" CREATED="1626724117029" ID="ID_772180534" MODIFIED="1626724119056" TEXT="References">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1626724124428" ID="ID_1480859679" MODIFIED="1626724133147" TEXT="Infrastructor as Code (IaC)"/>
+<node COLOR="#111111" CREATED="1626378126246" ID="ID_109165739" LINK="https://docs.aws.amazon.com/cloudformation/?id=docs_gateway" MODIFIED="1626379387196" TEXT="Cloud Formation">
+<node COLOR="#111111" CREATED="1626379487925" ID="ID_437642518" LINK="https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-template-resource-type-ref.html" MODIFIED="1626379516591" TEXT="Properties Reference"/>
+</node>
+<node COLOR="#111111" CREATED="1626725466247" ID="ID_67978719" LINK="https://docs.aws.amazon.com/whitepapers/latest/aws-overview/analytics.html" MODIFIED="1626725483590" TEXT="Overview of AWS Analytics Services"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1626724018782" ID="ID_1741031949" MODIFIED="1626724020401" TEXT="Concepts">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1626378108229" ID="ID_1986991416" MODIFIED="1626724023535" TEXT="IaC">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1626378116151" ID="ID_1839439847" MODIFIED="1626724023535" TEXT="Infrastructure as Code">
+<font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#111111" CREATED="1626378130317" ID="ID_765322187" MODIFIED="1626378131873" TEXT="YAML"/>
 <node COLOR="#111111" CREATED="1626378132413" ID="ID_1354653220" MODIFIED="1626378133753" TEXT="JSON"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1626378758592" ID="ID_64243880" LINK="https://aws.amazon.com/architecture/well-architected/?e=gs2020&amp;p=fundcore&amp;p=gsrc&amp;c=fcc" MODIFIED="1626379061405" TEXT="Well-Architected Framework">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1626379062657" ID="ID_391488620" MODIFIED="1626379066533" TEXT="Five Pillars">
+<node COLOR="#990000" CREATED="1626378758592" ID="ID_64243880" LINK="https://aws.amazon.com/architecture/well-architected/?e=gs2020&amp;p=fundcore&amp;p=gsrc&amp;c=fcc" MODIFIED="1626724024570" TEXT="Well-Architected Framework">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
-<node COLOR="#111111" CREATED="1626379068611" ID="ID_1992810401" MODIFIED="1626379078655" TEXT="Operational Excellence">
+<node COLOR="#111111" CREATED="1626379062657" ID="ID_391488620" MODIFIED="1626724024570" TEXT="Five Pillars">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1626379068611" FOLDED="true" ID="ID_1992810401" MODIFIED="1626725619070" TEXT="Operational Excellence">
 <node COLOR="#111111" CREATED="1626379144777" ID="ID_66412692" MODIFIED="1626379153669" TEXT="Continuously improve ability">
 <node COLOR="#111111" CREATED="1626379155057" ID="ID_745722824" MODIFIED="1626379156805" TEXT="run systems"/>
 <node COLOR="#111111" CREATED="1626379157065" ID="ID_1202309431" MODIFIED="1626379162232" TEXT="create better procedures"/>
@@ -201,13 +247,120 @@
 <node COLOR="#111111" CREATED="1626379493699" ID="ID_1789321461" LINK="https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html" MODIFIED="1626379530116" TEXT="API Reference"/>
 </node>
 </node>
-<node COLOR="#111111" CREATED="1626379296445" ID="ID_687351735" MODIFIED="1626379301710" TEXT="Observability"/>
+<node COLOR="#111111" CREATED="1626379296445" ID="ID_687351735" MODIFIED="1626379301710" TEXT="Observability">
+<node COLOR="#111111" CREATED="1626724232567" ID="ID_1396242497" MODIFIED="1626724234540" TEXT="measuring the internal state of your system"/>
+<node COLOR="#111111" CREATED="1626724261407" ID="ID_1579475553" MODIFIED="1626724262923" TEXT="optimize it to some desired end state."/>
+<node COLOR="#111111" CREATED="1626724271245" ID="ID_1676639583" MODIFIED="1626724275684" TEXT="Implementing">
+<node COLOR="#111111" CREATED="1626724277493" ID="ID_658622322" MODIFIED="1626724279817" TEXT="Collection">
+<node COLOR="#111111" CREATED="1626724301581" ID="ID_1979343267" MODIFIED="1626724304123" TEXT="aggregating all metrics necessary when assessing the state of your system"/>
+<node COLOR="#111111" CREATED="1626724336270" ID="ID_1158943858" MODIFIED="1626724338596" TEXT="Categories">
+<node COLOR="#111111" CREATED="1626724373310" ID="ID_538957608" MODIFIED="1626724404811" TEXT="Infrastructure-level">
+<node COLOR="#111111" CREATED="1626724428208" ID="ID_555051402" MODIFIED="1626724431658" TEXT="automatically by AWS services"/>
+<node COLOR="#111111" CREATED="1626724436704" ID="ID_1942972984" LINK="https://docs.aws.amazon.com/cloudwatch/?id=docs_gateway" MODIFIED="1626724519414" TEXT="collected by the CloudWatch service"/>
+<node COLOR="#111111" CREATED="1626724467432" ID="ID_405835471" MODIFIED="1626724470474" TEXT="Some services">
+<node COLOR="#111111" CREATED="1626724475230" ID="ID_641876888" MODIFIED="1626724492922" TEXT="emit structured logs"/>
+<node COLOR="#111111" CREATED="1626724482160" ID="ID_708915663" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/WhatIsCloudWatchLogs.html" MODIFIED="1626724505847" TEXT="enabled and collected through CloudWatch Logs"/>
 </node>
 </node>
-<node COLOR="#111111" CREATED="1626379079954" ID="ID_1998621402" MODIFIED="1626379086871" TEXT="Security"/>
+<node COLOR="#111111" CREATED="1626724378263" ID="ID_1636703570" MODIFIED="1626724410356" TEXT="Application-level">
+<node COLOR="#111111" CREATED="1626724546046" ID="ID_1593500710" MODIFIED="1626724548740" TEXT="generated by your software"/>
+<node COLOR="#111111" CREATED="1626724552952" ID="ID_132557544" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/publishingMetrics.html" MODIFIED="1626724564739" TEXT="collected by CloudWatch Custom Metrics"/>
+</node>
+<node COLOR="#111111" CREATED="1626724394471" ID="ID_1976084427" MODIFIED="1626724396544" TEXT="Account-level">
+<node COLOR="#111111" CREATED="1626724579008" ID="ID_172327275" MODIFIED="1626724580743" TEXT=" logged by your AWS account "/>
+<node COLOR="#111111" CREATED="1626724586535" ID="ID_929076433" LINK="https://docs.aws.amazon.com/cloudtrail/?id=docs_gateway" MODIFIED="1626724599379" TEXT="can be collected by the CloudTrail service"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724280069" ID="ID_1677741292" MODIFIED="1626724283691" TEXT="Analytics">
+<node COLOR="#111111" CREATED="1626724316071" ID="ID_634233995" MODIFIED="1626724322771" TEXT="analyze collected metrics"/>
+<node COLOR="#111111" CREATED="1626724673305" ID="ID_758908724" MODIFIED="1626724675341" TEXT="use case">
+<node COLOR="#111111" CREATED="1626724676825" ID="ID_985332111" MODIFIED="1626724714485" TEXT="logs stored in CloudWatch Logs">
+<node COLOR="#111111" CREATED="1626724761943" ID="ID_1512540510" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/AnalyzingLogData.html" MODIFIED="1626724774722" TEXT="CloudWatch Logs Insight">
+<node COLOR="#111111" CREATED="1626724789641" ID="ID_1198241571" MODIFIED="1626724792072" TEXT="interactively search and analyze"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724699495" ID="ID_1091080774" MODIFIED="1626724701940" TEXT="logs stored in S3">
+<node COLOR="#111111" CREATED="1626724801135" ID="ID_1613133789" LINK="https://aws.amazon.com/athena/?id=docs_gateway" MODIFIED="1626724825755" TEXT="Athena">
+<node COLOR="#111111" CREATED="1626724807914" ID="ID_1163368382" MODIFIED="1626724812451" TEXT="Serverless query server"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724708607" ID="ID_1446179133" MODIFIED="1626724710617" TEXT="structure data">
+<node COLOR="#111111" CREATED="1626724831296" ID="ID_381330228" LINK="https://docs.aws.amazon.com/rds/?id=docs_gateway" MODIFIED="1626724843448" TEXT="RDS">
+<node COLOR="#111111" CREATED="1626724856561" ID="ID_668979673" MODIFIED="1626724858878" TEXT="managed relational database service"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724722481" ID="ID_1760913557" MODIFIED="1626724737681" TEXT="large amounts of structured data">
+<node COLOR="#111111" CREATED="1626724864248" ID="ID_1166204557" LINK="https://docs.aws.amazon.com/redshift/?id=docs_gateway" MODIFIED="1626724886068" TEXT="Red Shift">
+<node COLOR="#111111" CREATED="1626724872000" ID="ID_1576992948" MODIFIED="1626724874693" TEXT="managed petabyte-scale data warehouse service"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724739528" ID="ID_458247288" MODIFIED="1626724741568" TEXT="log-based data">
+<node COLOR="#111111" CREATED="1626724896801" ID="ID_455320649" LINK="https://docs.aws.amazon.com/elasticsearch-service/?id=docs_gateway" MODIFIED="1626724919359" TEXT="ElasticSearch Service">
+<node COLOR="#111111" CREATED="1626724905248" ID="ID_1891970489" MODIFIED="1626724910523" TEXT="managed version of Elasticsearch"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626724284037" ID="ID_1987612105" MODIFIED="1626724285252" TEXT="Action">
+<node COLOR="#111111" CREATED="1626724952986" ID="ID_1424494249" MODIFIED="1626724964372" TEXT="After collection/analyzed metric">
+<node COLOR="#111111" CREATED="1626724969938" ID="ID_288045481" MODIFIED="1626724971960" TEXT="achieve a particular outcome or process"/>
+</node>
+<node COLOR="#111111" CREATED="1626724987913" ID="ID_1493831943" MODIFIED="1626725020374" TEXT="Outcomes &amp; processes">
+<node COLOR="#111111" CREATED="1626724975720" ID="ID_693005361" MODIFIED="1626724983254" TEXT="Monitoring &amp; alarming">
+<node COLOR="#111111" CREATED="1626725052563" ID="ID_211212935" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html" MODIFIED="1626725081980" TEXT="CloudWatch Alarms">
+<node COLOR="#111111" CREATED="1626725065747" ID="ID_1059352534" MODIFIED="1626725067952" TEXT="system has breached the safety threshold for a particular metric"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626725023722" ID="ID_782773947" MODIFIED="1626725026052" TEXT="Dashboards">
+<node COLOR="#111111" CREATED="1626725092130" ID="ID_666327890" LINK="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch_Dashboards.html" MODIFIED="1626725108261" TEXT="Cloudwatch Dashboards"/>
+</node>
+<node COLOR="#111111" CREATED="1626725026521" ID="ID_1008036824" MODIFIED="1626725034694" TEXT="Data-driven decisions">
+<node COLOR="#111111" CREATED="1626725121465" ID="ID_658511528" MODIFIED="1626725123761" TEXT="track performance and business KPIs "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626379079954" ID="ID_1998621402" MODIFIED="1626379086871" TEXT="Security">
+<node COLOR="#111111" CREATED="1626725676461" ID="ID_926340349" MODIFIED="1626725678395" TEXT="secure your infrastructure on the cloud">
+<node COLOR="#111111" CREATED="1626725702003" ID="ID_1847664801" MODIFIED="1626725703151" TEXT="AWS">
+<node COLOR="#111111" CREATED="1626725712676" ID="ID_1256772764" MODIFIED="1626725715079" TEXT="responsible for the security of the cloud"/>
+</node>
+<node COLOR="#111111" CREATED="1626725716428" ID="ID_1376450954" MODIFIED="1626725717889" TEXT="Customer">
+<node COLOR="#111111" CREATED="1626725739453" ID="ID_325858704" MODIFIED="1626725741697" TEXT="responsible for security in the cloud"/>
+<node COLOR="#111111" CREATED="1626725750197" ID="ID_1928293213" MODIFIED="1626725752322" TEXT="configuration">
+<node COLOR="#111111" CREATED="1626725754948" ID="ID_392518357" MODIFIED="1626725757264" TEXT="cloud services"/>
+<node COLOR="#111111" CREATED="1626725757460" ID="ID_137887366" MODIFIED="1626725764416" TEXT="application software"/>
+<node COLOR="#111111" CREATED="1626725765204" ID="ID_913539241" MODIFIED="1626725775392" TEXT="management of sensitive data"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1626725795716" ID="ID_34193694" MODIFIED="1626725804065" TEXT="Zero trust"/>
+</node>
+<node COLOR="#111111" CREATED="1626725827924" ID="ID_347701876" MODIFIED="1626725829553" TEXT="Concepts">
+<node COLOR="#111111" CREATED="1626725835134" ID="ID_427233463" MODIFIED="1626725837472" TEXT="Identity and Access Management (IAM)"/>
+<node COLOR="#111111" CREATED="1626725837908" ID="ID_1087464978" MODIFIED="1626725844186" TEXT="Network Security"/>
+<node COLOR="#111111" CREATED="1626725844486" ID="ID_687681597" MODIFIED="1626725849340" TEXT="Data Encryption"/>
+</node>
+</node>
 <node COLOR="#111111" CREATED="1626379091605" ID="ID_508317669" MODIFIED="1626379093573" TEXT="Reliability"/>
 <node COLOR="#111111" CREATED="1626379098915" ID="ID_1330399965" MODIFIED="1626379101064" TEXT="Performance Efficiency"/>
 <node COLOR="#111111" CREATED="1626379101932" ID="ID_1307873091" MODIFIED="1626379109816" TEXT="Cost Optimization"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1626465533958" ID="ID_1126009594" MODIFIED="1626724026353" TEXT="Investigate">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1626465520116" ID="ID_1228614827" MODIFIED="1626724026354" TEXT="Spot Instances">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1626465525726" ID="ID_1949533421" MODIFIED="1626724026355" TEXT="ALB:  Application Load Balancer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
 </node>
